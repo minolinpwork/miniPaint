67cf49ea6ec5a19f8b4a1b816056137d5988e750
minolinpwork <minolinp.work@gmail.com>
2022-06-28 08:43:13 +0200

    b

--- src/js/modules/file/save.js
+++ src/js/modules/file/save.js
@@ -593,7 +593,7 @@ class File_save_class {
 				width: config.WIDTH,
 				height: config.HEIGHT,
 				dither: 'FloydSteinberg-serpentine',
-				workerScript: 'https://storage.googleapis.com/minipaint-icon-1/src/js/libs/gifjs/gif.worker.js',
+				workerScript: '/src/js/libs/gifjs/gif.worker.js',
 			};
 			if (config.TRANSPARENCY == true) {
 				gif_settings.transparent = 'rgba(0,0,0,0)';
@@ -784,7 +784,7 @@ class File_save_class {
 				width: config.WIDTH,
 				height: config.HEIGHT,
 				dither: 'FloydSteinberg-serpentine',
-				workerScript: 'https://storage.googleapis.com/minipaint-icon-1/src/js/libs/gifjs/gif.worker.js',
+				workerScript: '/src/js/libs/gifjs/gif.worker.js',
 			};
 			if (config.TRANSPARENCY == true) {
 				gif_settings.transparent = 'rgba(0,0,0,0)';
